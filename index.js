05d1dbea27a1a7e4e47288bd1e2dd232905b4034
QuinttAdam <116001984+QuinttAdam@users.noreply.github.com>
2023-10-18 21:54:11 +0200

    feat: add post to api

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
 
 //enable CORS
 const cors = require('cors');
 app.use(cors());
+
+
+app.use(express.json());
 
 
 app.get('/api/v1/messages', (req, res) => {
@@ -16,7 +19,7 @@ app.get('/api/v1/messages', (req, res) => {
     res.json({
       "status": "success",
       "message": `GET message for username ${username}`,
-    })
+    });
   }else{
     res.json({
       "status": "success",
@@ -31,9 +34,9 @@ app.get('/api/v1/messages', (req, res) => {
           "message": "Goodbye World"
         }
       ],
-    })
+    });
   }
-})
+});
 app.get('/api/v1/messages/:id', (req, res) => {
   // Get the ID from the URL
   const id = req.params.id;  
@@ -48,8 +51,19 @@ app.get('/api/v1/messages/:id', (req, res) => {
         "message": "Goodbye World"
       }
     ],
-  })
-})
+  });
+});
+app.post('/api/v1/messages', (req, res) => {
+  // Get the user from the body
+  const user = req.body.message.user;
+  
+  res.json({
+    
+    "status": "success",
+    "message": `POSTING new message for user ${user}`,
+    
+  });
+});
 app.put('/api/v1/messages/:id', (req, res) => {
   // Get the ID from the URL
   const id = req.params.id;  
@@ -59,8 +73,8 @@ app.put('/api/v1/messages/:id', (req, res) => {
     "status": "success",
     "message": `UPDATE message with id ${id}`,
     
-  })
-})
+  });
+});
 app.delete('/api/v1/messages/:id', (req, res) => {
   // Get the ID from the URL
   const id = req.params.id;  
@@ -70,10 +84,10 @@ app.delete('/api/v1/messages/:id', (req, res) => {
     "status": "success",
     "message": `DELETING message with id ${id}`,
     
-  })
-})
+  });
+});
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
